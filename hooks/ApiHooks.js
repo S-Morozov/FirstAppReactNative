9083f3a96aa53f1a2470acd81929612b839331d6
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-09-01 17:16:40 +0300

    #updatted branch

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import {apiUrl} from '../utils/appConfig';
+import {apiUrl} from '../utils/app-config';
 import {doFetch} from '../utils/functions';
 
 const useMedia = () => {
@@ -30,4 +30,46 @@ const useMedia = () => {
   return {mediaArray};
 };
 
-export {useMedia};
+const useAuthentication = () => {
+  const postLogin = async (user) => {
+    console.log(user);
+    try {
+      return await doFetch(apiUrl + 'login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+    } catch (error) {
+      console.error('postLogin error', error);
+    }
+  };
+
+  return {postLogin};
+};
+
+const useUser = () => {
+  const getUserByToken = async (token) => {
+    const options = {
+      method: 'GET',
+      headers: {'x-access-token': token},
+    };
+    return await doFetch(apiUrl + 'users/user', options);
+  };
+
+  const postUser = async (userData) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    };
+    return await doFetch(apiUrl + 'users', options);
+  };
+
+  return {getUserByToken, postUser};
+};
+
+export {useMedia, useAuthentication, useUser};
--- views/Login.js
+++ views/Login.js
@@ -1,22 +1,35 @@
 import React, {useContext, useEffect} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useUser} from '../hooks/ApiHooks';
+import LoginForm from '../components/LoginForm';
 
 const Login = ({navigation}) => {
   // props is needed for navigation
-  const {setIsLoggedIn} = useContext(MainContext);
+  const {setIsLoggedIn, setUser} = useContext(MainContext);
+  const {getUserByToken} = useUser();
 
   const checkToken = async () => {
     try {
       const token = await AsyncStorage.getItem('userToken');
-      // token validation
-      if (token === 'abcdef') {
+      // hardcoded token validation
+      const userData = await getUserByToken(token);
+      console.log('userdata', userData);
+      if (userData) {
         setIsLoggedIn(true);
+        setUser(userData);
       }
     } catch (error) {
-      console.log(error);
+      console.log('checkToken', error);
     }
   };
 
@@ -24,20 +37,20 @@ const Login = ({navigation}) => {
     checkToken();
   }, []);
 
-  const logIn = async () => {
-    console.log('Button pressed');
-    try {
-      setIsLoggedIn(true);
-      await AsyncStorage.setItem('userToken', 'abc');
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <Button title="Sign in!" onPress={logIn} />
-    </View>
+    <TouchableOpacity
+      onPress={() => Keyboard.dismiss()}
+      style={{flex: 1}}
+      activeOpacity={1}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+      >
+        <Text>Login</Text>
+        <LoginForm />
+      </KeyboardAvoidingView>
+    </TouchableOpacity>
   );
 };
 
